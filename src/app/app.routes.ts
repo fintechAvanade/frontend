2086a9aa1063f81d626a290b88e3b2696ae41e7c
Rodrigo Resende Viana <rodrigo.viana@avanade.com>
2025-01-21 15:31:58 -0300

    Adicionando base de rotas e componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { LoginComponent } from './pages/login/login.component';
+import { CadastroComponent } from './pages/cadastro/cadastro.component';
+import { AdminHomeComponent } from './components/admin-home/admin-home.component';
+import { ClienteHomeComponent } from './components/cliente-home/cliente-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '/', component: LandingPageComponent},
+    {path: '/login', component: LoginComponent},
+    {path: '/cadastro', component: CadastroComponent},
+    {
+        path: '/cliente', 
+        component: CadastroComponent,
+        children:[
+            { path: 'home', component: ClienteHomeComponent },
+        ]
+    },
+    {
+        path: '/admin', 
+        component: CadastroComponent,
+        children:[
+            { path: 'home', component: AdminHomeComponent },
+        ]
+    }
+];
