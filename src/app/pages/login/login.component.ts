394df1ae7c5267448b517c34a761eea3ffecbe9e
Your Name <thaisaksr@gmail.com>
2025-01-23 15:42:08 -0300

    atualizacao do login e button filled

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { ButtonFilledComponent } from '../../shared/button-filled/button-filled.component';
 
 @Component({
   selector: 'app-login',
+  imports: [ButtonFilledComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'], // Opcional, mas pode incluir Tailwind aqui se precisar de classes globais
 })
---
+++ src/app/shared/button-filled/button-filled.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-button-filled',
+  imports: [],
+  templateUrl: './button-filled.component.html',
+  styleUrl: './button-filled.component.css'
+})
+export class ButtonFilledComponent {
+
+}
