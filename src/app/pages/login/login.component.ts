0467937f0888c9128ee6d161771effc4b89e9826
Your Name <thaisaksr@gmail.com>
2025-01-23 19:59:21 -0300

    secundary button adicionado, e atualizacao do login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonFilledComponent } from '../../shared/button-filled/button-filled.component';
+import { SecundaryButtonComponent } from '../../shared/secundary-button/secundary-button.component';
 
 @Component({
   selector: 'app-login',
-  imports: [ButtonFilledComponent],
+  imports: [ButtonFilledComponent,SecundaryButtonComponent],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'], // Opcional, mas pode incluir Tailwind aqui se precisar de classes globais
 })
