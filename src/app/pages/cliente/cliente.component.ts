683d7bca78776895baf1eb73f9642fac7aca5a45
Rodrigo Resende Viana <rodrigo.viana@avanade.com>
2025-01-24 08:48:19 -0300

    iniciando navbar

--- src/app/pages/cliente/cliente.component.ts
+++ src/app/pages/cliente/cliente.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from '../../components/navbar/navbar.component';
 
 @Component({
   selector: 'app-cliente',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './cliente.component.html',
   styleUrl: './cliente.component.css'
 })
 export class ClienteComponent {
-
+  
 }
