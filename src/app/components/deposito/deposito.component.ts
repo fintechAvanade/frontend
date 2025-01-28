185c2b330549ffd693e430ba9e7e6af8c1387872
Your Name <thaisaksr@gmail.com>
2025-01-28 11:59:28 -0300

    atualizacao da tela deposito- criacao do metodo para pegar o sado na API

--- src/app/components/deposito/deposito.component.ts
+++ src/app/components/deposito/deposito.component.ts
@@ -1,16 +1,33 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { Router } from '@angular/router';  
+// import { SaldoService } from './saldo.service';  
+// // Falta Importar o serviço que criamos para o Saldo.
 
 @Component({
   selector: 'app-deposito',
   templateUrl: './deposito.component.html',
   styleUrls: ['./deposito.component.css']  
 })
-export class DepositoComponent {
+export class DepositoComponent implements OnInit{
   // Variável que armazena o valor do depósito
   valorInput: number = 0;
 
-  constructor(private router: Router) {}
+
+
+  constructor(private router: Router, private saldoService: SaldoService) {}
+
+  ngOnInit(): void {
+  this.obterSaldo();
+  }
+  saldo: number = 0;
+
+  obterSaldo(): void {
+    this.saldoService.obterSaldo().subscribe( //subscribe permite que você "escute" a resposta da API e faça algo com ela (como salvar o valor em uma variável).
+      (data) => { //quando a requisicao foi bem-sucedida
+        this.saldo = data.saldo;  // Assume que o saldo vem dentro de uma chave "saldo" da api
+      },
+  }
+
 
   // Método para adicionar valores ao depósito
   adicionarValor(valor: number): void {
@@ -26,3 +43,5 @@ export class DepositoComponent {
     }
   }
 }
+
+//falta criar o service!!!
