bd8bd059cd20af3d098ef1fca8be79fcdfe2658f
Your Name <thaisaksr@gmail.com>
2025-01-29 18:16:47 -0300

    Inicio da criacao da tela, saque, boleto e transferencia

--- src/app/components/saque/saque.component.ts
+++ src/app/components/saque/saque.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router'; 
+import { SaldoComponent } from '../../shared/saldo/saldo.component';
+  
+  @Component({
+    selector: 'app-saque',
+    imports: [FormsModule, SaldoComponent],
+    templateUrl: './saque.component.html',
+    styleUrls: ['./saque.component.css']  
+  })
+  export class SaqueComponent implements OnInit{
+    // Variável que armazena o valor do depósito
+    valorInput: number = 0;
 
-@Component({
-  selector: 'app-saque',
-  imports: [],
-  templateUrl: './saque.component.html',
-  styleUrl: './saque.component.css'
-})
-export class SaqueComponent {
+    constructor(private router: Router) {}
 
-}
+    ngOnInit(): void {
+
+    }
+    saldo: number = 0;
+
+    // Método para adicionar valores ao saque
+    saqueValor(valor: number): void {
+    
+      this.valorInput += valor; 
+    }
+      
+    // Método para confirmar o saque
+    confirmarSaque(): void {
+      if (this.valorInput > 0) {
+        this.router.navigate(['/transacao']); // falar com o professor como colocar uma rota que coloque que foi efetuado o deposito, e depois apagassse e continuasse na home.
+      } else {
+        alert('Por favor, insira um valor válido!');
+      }
+    }
+  }
