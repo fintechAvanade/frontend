96bfb2ccbd32405e4b4b81dd2e25d55ee44ebfe8
Your Name <thaisaksr@gmail.com>
2025-01-31 12:32:58 -0300

    Tela pagamento de boletos

--- src/app/components/pagamento-boletos/pagamento-boletos.component.ts
+++ src/app/components/pagamento-boletos/pagamento-boletos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router'; 
 import { SaldoComponent } from '../../shared/saldo/saldo.component';
@@ -10,6 +10,20 @@ import { SaldoComponent } from '../../shared/saldo/saldo.component';
   templateUrl: './pagamento-boletos.component.html',
   styleUrl: './pagamento-boletos.component.css'
 })
-export class PagamentoBoletosComponent{} 
+export class PagamentoBoletosComponent implements OnInit{
+  router: any;
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
 
+  valorBoleto: number = 0;
 
+    // Método para confirmar o depósito
+    confirmarPagamento(): void {
+      if (this.valorBoleto > 0) {
+        this.router.navigate(['/transacao']);
+      } else {
+        alert('Por favor, insira um valor válido!');
+      }
+    }
+}
